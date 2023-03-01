f37ac120f01a1cce9068200ef5bed79b28d790c3
ianmah.eth <ianmmah@gmail.com>
2023-03-01 06:10:56 +0000

    put data in props

--- pages/profile/[username].jsx
+++ pages/profile/[username].jsx
@@ -1,6 +1,5 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import axios from 'axios'
-import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import Container from '../../components/Container'
@@ -8,15 +7,34 @@ import Footer from '../../components/Footer'
 import { API_URL } from '../../util/api'
 
 export async function getServerSideProps() {
-  const res = await axios.get(API_URL + "/api/user", {
-  })
+  const res = await axios.get(API_URL + "/api/user", {})
+
+  const {
+    name,
+    followers_count,
+    following_count,
+    profile_picture_url,
+    wallet_address
+  } = res.data.talent
   
   return {
-    props: {}, // will be passed to the page component as props
+    props: {
+      name,
+      followers_count,
+      following_count,
+      profile_picture_url,
+      wallet_address
+    }
   }
 }
 
-const Profile = () => {
+const Profile = ({
+      name,
+      followers_count,
+      following_count,
+      profile_picture_url,
+      wallet_address
+    }) => {
   const router = useRouter()
   const { username } = router.query
 
@@ -27,7 +45,7 @@ const Profile = () => {
       <Head>
         <title>{title}</title>
         <meta
-          name="description"
+          name={title}
           content="Talent Protocol x Lens social app"
         />
         <link rel="icon" href="/favicon.ico" />
@@ -37,7 +55,7 @@ const Profile = () => {
         <ConnectButton />
 
         <h1>
-          {username}
+          {username} {name}
         </h1>
       </main>
 
