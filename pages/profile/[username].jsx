13a69fde4e8df2952c8f2b36a7b4a39667745de7
ianmah.eth <ianmmah@gmail.com>
2023-03-01 07:29:18 +0000

    get lens from talent wallet

--- pages/profile/[username].jsx
+++ pages/profile/[username].jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import { useLazyQuery, gql } from '@apollo/client'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import axios from 'axios'
 import { useRouter } from 'next/router'
@@ -6,37 +8,140 @@ import Container from '../../components/Container'
 import Footer from '../../components/Footer'
 import { API_URL } from '../../util/api'
 
-export async function getServerSideProps(context) {
-  const res = await axios.get(`${API_URL}/api/talent/${context.query.username}`, {})
-
-  const {
-    name,
-    followers_count,
-    following_count,
-    profile_picture_url,
-    wallet_address
-  } = res.data.talent
-  
-  return {
-    props: {
-      name,
-      followers_count,
-      following_count,
-      profile_picture_url,
-      wallet_address
+// TODO: Move to query file
+const GET_PROFILES = gql`
+query($request: ProfileQueryRequest!) {
+  profiles(request: $request) {
+    items {
+      id
+      name
+      bio
+      attributes {
+        displayType
+        traitType
+        key
+        value
+      }
+      followNftAddress
+      metadata
+      isDefault
+      picture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      handle
+      coverPicture {
+        ... on NftImage {
+          contractAddress
+          tokenId
+          uri
+          verified
+        }
+        ... on MediaSet {
+          original {
+            url
+            mimeType
+          }
+        }
+        __typename
+      }
+      ownedBy
+      dispatcher {
+        address
+        canUseRelay
+      }
+      stats {
+        totalFollowers
+        totalFollowing
+        totalPosts
+        totalComments
+        totalMirrors
+        totalPublications
+        totalCollects
+      }
+      followModule {
+        ... on FeeFollowModuleSettings {
+          type
+          amount {
+            asset {
+              symbol
+              name
+              decimals
+              address
+            }
+            value
+          }
+          recipient
+        }
+        ... on ProfileFollowModuleSettings {
+         type
+        }
+        ... on RevertFollowModuleSettings {
+         type
+        }
+      }
+    }
+    pageInfo {
+      prev
+      next
+      totalCount
     }
   }
-}
+}`
 
-const Profile = ({
-      name,
-      followers_count,
-      following_count,
-      profile_picture_url,
-      wallet_address
-    }) => {
+const Profile = ({}) => {
   const router = useRouter()
   const { username } = router.query
+  const [talentProfile, setTalentProfile] = useState({})
+  const [lensProfile, setLensProfile] = useState({})
+  const [getProfiles, { loading, error, data }] = useLazyQuery(GET_PROFILES, {
+    variables: {
+      request: {
+          ownedBy: [talentProfile.wallet_address],
+          limit: 1,
+      },
+    }
+  })
+
+  useEffect(() => {
+    if (!username) return;
+    const getData = async () => {
+      const res = await axios.get(`${API_URL}/api/talent/${username}`, {})
+      const {
+        name,
+        followers_count,
+        following_count,
+        profile_picture_url,
+        wallet_address
+      } = res.data.talent
+  
+      setTalentProfile({
+        name,
+        followers_count,
+        following_count,
+        profile_picture_url,
+        wallet_address
+      })
+    }
+    getData()
+  }, [setTalentProfile, getProfiles, username])
+
+  useEffect(() => {
+    if (talentProfile.wallet_address) {
+      getProfiles()
+    }
+  }, [getProfiles, talentProfile.wallet_address])
 
   const title = `${username} ✦ Talentlens`
 
@@ -55,8 +160,9 @@ const Profile = ({
         <ConnectButton />
 
         <h1>
-          {username} {name}
+          {username} {talentProfile.name}
         </h1>
+        <p>{data && JSON.stringify(data)}</p>
       </main>
 
       <Footer />
