f5a7e8ee4edb629783f53898e9b8dd908d017603
ianmah.eth <ianmmah@gmail.com>
2023-03-02 21:33:38 +0000

    _app contains providers again

--- components/Web3Providers.jsx
+++ components/Web3Providers.jsx
@@ -5,56 +5,12 @@ import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { polygon } from 'wagmi/chains'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
-import ThemeProvider from '../styles/ThemeProvider'
-import GlobalStyles from '../styles/GlobalStyle'
 
 function Providers({ children }) {
-  const [wagmiClient, setWagmiClient] = useState()
-  const [chains, setChains] = useState()
-
-  useEffect(() => {
-    const { chains, provider, webSocketProvider } = configureChains(
-      [
-        polygon,
-      ],
-      [
-        alchemyProvider({
-          apiKey: process.env.ALCHEMY_KEY || '',
-        }),
-        publicProvider(),
-      ]
-    );
-    
-    const { connectors } = getDefaultWallets({
-      appName: 'Talent Lens',
-      chains,
-    });
-    
-    const wagmiClient = createClient({
-      autoConnect: true,
-      connectors,
-      provider,
-      webSocketProvider,
-    });
-
-    setWagmiClient(wagmiClient)
-    setChains(chains)
-
-  }, [setWagmiClient, setChains])
-
-  if (!wagmiClient) {
-    return <p>loading</p>    
-  }
 
   return (
-    <WagmiConfig client={wagmiClient}>
-      <ThemeProvider>
-        <GlobalStyles />
-          <RainbowKitProvider chains={chains}>
-            { children }
-          </RainbowKitProvider>
-      </ThemeProvider>
-    </WagmiConfig>
+
+           <>{ children }</>
   );
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,9 @@
+import { useEffect, useState } from 'react'
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
+import { polygon } from 'wagmi/chains'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
 import type { AppProps } from 'next/app'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 
@@ -10,14 +16,54 @@ const client = new ApolloClient({
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [wagmiClient, setWagmiClient] = useState()
+  const [chains, setChains] = useState()
+
+  useEffect(() => {
+    const { chains, provider, webSocketProvider } = configureChains(
+      [
+        polygon,
+      ],
+      [
+        alchemyProvider({
+          apiKey: process.env.ALCHEMY_KEY || '',
+        }),
+        publicProvider(),
+      ]
+    );
+    
+    const { connectors } = getDefaultWallets({
+      appName: 'Talent Lens',
+      chains,
+    });
+    
+    const wagmiClient = createClient({
+      autoConnect: true,
+      connectors,
+      provider,
+      webSocketProvider,
+    });
+
+    setWagmiClient(wagmiClient)
+    setChains(chains)
+
+  }, [setWagmiClient, setChains])
+
+  if (!wagmiClient) {
+    return <p>loading</p>    
+  }
 
   return (
+    <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>
     <ApolloProvider client={client}>
     <ThemeProvider>
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
     </ApolloProvider>
+    </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
 
